0a522fa5a74d17d9a0d6df21c09003030d252280
Kameel <kameel.ahamed@gmail.com>
2026-07-22 15:50:31 -0400

    per-country score extraction from wave datasets; added caching for large reads and reference data

--- helpers.py
+++ helpers.py
@@ -13,6 +13,16 @@ class DocOutput(TypedDict):
     questions: list[str]
     q_ids: list[str]
 
+class CountryScores(TypedDict):
+    total: int
+    total_agree: int
+    total_disagree: int
+    totally_agree: int
+    tend_to_agree: int
+    dont_know: int
+    tend_to_disagree: int
+    totally_disagree: int
+
 EB_BASE_PATH = "data/raw/"
 
 COUNTRY_CODES = {
@@ -52,6 +62,7 @@ def check_q(q: str) -> bool:
     or imposing economic sanctions on Russia.
     """
     q = q.lower()
+
     return (
         ("ukraine" in q and "military" in q)
         or
@@ -68,6 +79,7 @@ def _parse_dates(ds: str) -> tuple[date, date, str]:
     end = ds.split("-")[-1].strip()
     fw_start = date(*[int(v) for v in start.split("/")[::-1]])
     fw_end = date(*[int(v) for v in end.split("/")[::-1]])
+
     if any(m == fw_start.month for m in [3, 4, 5]):
         season = f"Spring {fw_start.year}"
     elif any(m == fw_start.month for m in [6, 7, 8]):
@@ -76,6 +88,7 @@ def _parse_dates(ds: str) -> tuple[date, date, str]:
         season = f"Fall {fw_start.year}"
     elif any(m == fw_start.month for m in [12, 1, 2]):
         season = f"Spring {fw_start.year}"
+
     return (fw_start, fw_end, season)
 
 def parse_doc(fn: str) -> DocOutput:
@@ -88,11 +101,13 @@ def parse_doc(fn: str) -> DocOutput:
     wave_id = float(df.iat[0, 1])  # type: ignore[arg-type]
     fw_start, fw_end, season = _parse_dates(str(df.iat[1, 1]))
     col_qs, col_ids = [], []
+
     for question in df["Unnamed: 2"][4:].dropna():
         q_n: str = question.split(". ", 1)[0]
         if check_q(question):
             col_qs.append(question)
             col_ids.append(q_n.replace(".", "_"))
+
     return DocOutput(
         eb_n=eb_n,
         wave_id=wave_id,
@@ -102,3 +117,45 @@ def parse_doc(fn: str) -> DocOutput:
         questions=col_qs,
         q_ids=col_ids
     )
+
+def collect_scores(wave: str, q_ids: list[str]) -> dict[str, dict[str, int]]:
+    """
+    Collect per-country responses to questions with question numbers *q_ids*
+    in Eurobarometer wave *wave*.
+    """
+    collected = {}
+
+    df = pd.read_excel(
+        f"{EB_BASE_PATH}{wave}_volume_A.xlsx",
+        sheet_name=None,
+        header=7,
+    )
+
+    for q in q_ids:
+        q_col = {}
+
+        sdf = df[q].dropna(how="all")
+        sdf = sdf.drop(sdf.columns[0], axis=1)
+        countries = sdf.iloc[0][2:].to_list()
+        sdf.columns = ["Score", "EU27"] + countries
+
+        for country in countries:
+            if country not in COUNTRY_CODES:
+                # Skip surveyed countries that are not EU member states and
+                # regional divisions absorbed by fixed effects.
+                continue
+            cdf = sdf.set_index("Score")[country]
+            q_col[COUNTRY_CODES[country]] = CountryScores(
+                total=cdf.at["Total"],
+                total_agree=cdf.at["Total 'D'accord'"],
+                total_disagree=cdf.at["Total 'Pas d'accord'"],
+                totally_agree=cdf.at["Tout à fait d'accord"],
+                tend_to_agree=cdf.at["Plutôt d'accord"],
+                dont_know=cdf.at["Ne sait pas"],
+                tend_to_disagree=cdf.at["Plutôt pas d'accord"],
+                totally_disagree=cdf.at["Pas du tout d'accord"]
+            )
+
+        collected[q] = q_col
+
+    return collected
